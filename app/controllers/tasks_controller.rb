1dbc99092bd31caa23d6176e455fcb61a403b239
yumeka.nosaka <yumeka.nosaka.isa@gmail.com>
2020-07-19 06:17:27 +0000

    usertasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,27 @@
 class TasksController < ApplicationController
-  before_action  :require_user_logged_in
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
   
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
  
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end   
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:sucess] = "保存されました！"
-      redirect_to task_url(@task)
+      redirect_to tasks_url
     else
       flash.now[:danger] = "保存されませんでした！"
       render :new
@@ -27,11 +30,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
       flash[:sucess] ="更新されました！"
       redirect_to task_url(@task)
@@ -42,9 +45,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
     flash[:sucess] = "削除されました！"
     redirect_to tasks_url
   
@@ -56,4 +57,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content,:status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to "/"
+    end
+  end
 end
